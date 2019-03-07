9d3c8aed88176e740d133d4bce3854b1ca885701
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-07 19:59:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -320,7 +320,7 @@ client.channels.get("543858517832892442").sendEmbed(embed)
 
 client.on('ready', function(){
     var ms = 10000 ;[
-    var setGame = ['In 596 Usr','*help | *bc',In Server 3,'By:Mehdi.!Øps |',channels! 21;]
+    var setGame = ['In 596 Usr','*help | *bc',In Server 3,'By:Mehdi.!Øps |',channels! 21;
     var i = -1;
     var j = 0;
     setInterval(function (){
